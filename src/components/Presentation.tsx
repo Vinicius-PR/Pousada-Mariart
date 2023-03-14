c6b29c2c184d7f7c055ab11e150b7e3a61e2546e
Vinicius <vinicius_auto@outlook.com>
2023-03-14 11:32:55 -0300

    fixing typo

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -5,7 +5,7 @@ export default function Presentation() {
     <section className="presentation">
       <div className="presentation__content">
         <h1>Pousada Mariart</h1>
-        <h2>Um das melhores da região</h2>
+        <h2>A melhor da região</h2>
         <h3>Venha aproveitar e conhecer a cidade do artesanato. Com muitas lojas e atrações a noite, você encontra aqui em Resende Costa.</h3>
       </div>
     </section>
